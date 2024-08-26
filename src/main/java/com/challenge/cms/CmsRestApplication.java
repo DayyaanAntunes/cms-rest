dcab391b993e8cd371a58def43a2c6bf0708ae58
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-08-26 10:28:13 +0200

    initial commit

---
+++ src/main/java/com/challenge/cms/CmsRestApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/cms/CmsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
