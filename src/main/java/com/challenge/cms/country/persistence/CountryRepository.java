94a36d276b0d77cdd026438984ec894261477fc9
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-08-27 09:00:56 +0200

    feat(country): add country repository

---
+++ src/main/java/com/challenge/cms/country/persistence/CountryRepository.java
@@ -0,0 +1,13 @@
+package com.challenge.cms.country.persistence;
+
+import com.challenge.cms.country.domain.model.Country;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    List<Country> findAll(Sort sort);
+}
