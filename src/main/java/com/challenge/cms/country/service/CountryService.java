f5d2a8b6811476e97464dc442b010eb5a12e406f
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-08-27 09:01:12 +0200

    feat(country): add country service

---
+++ src/main/java/com/challenge/cms/country/service/CountryService.java
@@ -0,0 +1,14 @@
+package com.challenge.cms.country.service;
+
+import com.challenge.cms.country.domain.command.CountryCommand;
+import com.challenge.cms.country.domain.model.Country;
+
+import java.util.List;
+
+public interface CountryService {
+    List<Country> findAll(String sortBy, String direction);
+    Country create(CountryCommand countryCommand);
+    Country update(Long id, CountryCommand countryCommand);
+    Country findById(Long id);
+    void delete(Long id);
+}
