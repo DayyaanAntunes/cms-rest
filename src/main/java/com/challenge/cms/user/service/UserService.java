d6871097244963975c11b8af3c72259505f6de00
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 01:35:29 +0200

    feat(user): implement loadUserByUsername in UserService

--- src/main/java/com/challenge/cms/user/service/UserService.java
+++ src/main/java/com/challenge/cms/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.challenge.cms.user.service;
 
+import com.challenge.cms.user.presentation.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Autowired
+    UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
     }
 }
