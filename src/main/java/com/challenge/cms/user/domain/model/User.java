fd9885fd91c53a2cc178632db4e8e8f6504c145a
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 01:10:05 +0200

    feat(user): setup User class with necessary fields

--- src/main/java/com/challenge/cms/user/domain/model/User.java
+++ src/main/java/com/challenge/cms/user/domain/model/User.java
@@ -1,5 +1,23 @@
 package com.challenge.cms.user.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 }
