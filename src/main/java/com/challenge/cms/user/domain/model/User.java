70f4b086215c8836c5a25b53994412ce607cf5ed
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 01:23:57 +0200

    ref(user): update UserDetails implemented methods

--- src/main/java/com/challenge/cms/user/domain/model/User.java
+++ src/main/java/com/challenge/cms/user/domain/model/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -28,12 +29,20 @@ public class User implements UserDetails {
     private Role role;
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
+    public Collection<? extends GrantedAuthority> getAuthorities() {return List.of(new SimpleGrantedAuthority(role.name()));}
 
     @Override
-    public String getUsername() {
-        return getEmail();
-    }
+    public String getUsername() {return getEmail();}
+
+    @Override
+    public boolean isAccountNonExpired() {return true;}
+
+    @Override
+    public boolean isAccountNonLocked() {return true;}
+
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
+
+    @Override
+    public boolean isEnabled() {return true;}
 }
