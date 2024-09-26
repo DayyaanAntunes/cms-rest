c67af84fbb4a6e6c157390eb2a7e4041106b89c2
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 02:34:47 +0200

    feat(user): add user existence verification by email

--- src/main/java/com/challenge/cms/user/presistence/UserRepository.java
+++ src/main/java/com/challenge/cms/user/presistence/UserRepository.java
@@ -1,8 +1,9 @@
-package com.challenge.cms.user.presentation;
+package com.challenge.cms.user.presistence;
 
 import com.challenge.cms.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    boolean existsByEmail(String email);
 }
