f1eb72417e7fc18ab1c748ae64cf3b56bd8b9f19
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 02:57:18 +0200

    feat(user): add findById method to UserServiceInterface

--- src/main/java/com/challenge/cms/user/service/UserServiceInterface.java
+++ src/main/java/com/challenge/cms/user/service/UserServiceInterface.java
@@ -6,4 +6,5 @@ import com.challenge.cms.user.domain.model.User;
 public interface UserServiceInterface {
     User save(UserCommand userCommand);
     User update(UserCommand userCommand);
+    User findById(Long id);
 }
