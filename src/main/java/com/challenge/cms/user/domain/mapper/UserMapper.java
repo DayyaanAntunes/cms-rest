e45fbc678ad7038b99f2216c058525ac9f4d3992
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 02:48:34 +0200

    feat(user): create Json for user response

--- src/main/java/com/challenge/cms/user/domain/mapper/UserMapper.java
+++ src/main/java/com/challenge/cms/user/domain/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package com.challenge.cms.user.domain.mapper;
 
 import com.challenge.cms.user.domain.command.UserCommand;
 import com.challenge.cms.user.domain.model.User;
+import com.challenge.cms.user.presentation.UserJson;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User toUser (UserCommand userCommand);
+    UserJson toUserJson (User user);
 }
