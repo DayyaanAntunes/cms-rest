83cbf1e74162cd71a15e02203edb3b96f5b29dd8
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 04:41:47 +0200

    feat(country): add toJsonLimited to convert list of countries to list of country json with limited data

--- src/main/java/com/challenge/cms/country/domain/mapper/CountryMapper.java
+++ src/main/java/com/challenge/cms/country/domain/mapper/CountryMapper.java
@@ -3,6 +3,7 @@ package com.challenge.cms.country.domain.mapper;
 import com.challenge.cms.country.domain.command.CountryCommand;
 import com.challenge.cms.country.domain.model.Country;
 import com.challenge.cms.country.presentation.Json.CountryJson;
+import com.challenge.cms.country.presentation.Json.CountryJsonLimited;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface CountryMapper {
 
+    List<CountryJsonLimited> toJsonLimited(List<Country> country);
     CountryJson toJson(Country country);
     List<CountryJson> toJson(List<Country> countries);
     Country toCountry(CountryCommand countryCommand);
