5b1d6bbdb52bcc093eefaf4b6e0160cde27f9655
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 04:27:39 +0200

    feat(security): add custom SecurityFilter

--- src/main/java/com/challenge/cms/infra/security/SecurityConfigurations.java
+++ src/main/java/com/challenge/cms/infra/security/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package com.challenge.cms.infra.security;
 
+import lombok.AllArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfigurations {
+
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -26,6 +32,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST,"/api/users").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
