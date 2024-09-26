613dfffbe9a63f7a9b6d931788e765c5ac8e39f5
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 02:57:31 +0200

    feat(user): add findAll method to UserServiceInterface

--- src/main/java/com/challenge/cms/user/service/UserServiceInterface.java
+++ src/main/java/com/challenge/cms/user/service/UserServiceInterface.java
@@ -2,9 +2,11 @@ package com.challenge.cms.user.service;
 
 import com.challenge.cms.user.domain.command.UserCommand;
 import com.challenge.cms.user.domain.model.User;
+import java.util.List;
 
 public interface UserServiceInterface {
     User save(UserCommand userCommand);
     User update(UserCommand userCommand);
     User findById(Long id);
+    List<User> findAll();
 }
