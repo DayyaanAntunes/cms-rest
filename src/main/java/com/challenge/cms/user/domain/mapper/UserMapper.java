38b318c19d5947d6983b84146029945ede3de6f4
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-26 04:18:15 +0200

    feat(user): add method to update User from command

--- src/main/java/com/challenge/cms/user/domain/mapper/UserMapper.java
+++ src/main/java/com/challenge/cms/user/domain/mapper/UserMapper.java
@@ -4,10 +4,12 @@ import com.challenge.cms.user.domain.command.UserCommand;
 import com.challenge.cms.user.domain.model.User;
 import com.challenge.cms.user.presentation.UserJson;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User toUser (UserCommand userCommand);
     UserJson toUserJson (User user);
+    void toModel(UserCommand userCommand,@MappingTarget User user);
 }
