e741cdaf1f9697078717f55de490d170c64abe02
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-29 22:42:42 +0200

    feat(country): implement soft delete with isDeleted attribute and annotations

--- src/main/java/com/challenge/cms/country/domain/model/Country.java
+++ src/main/java/com/challenge/cms/country/domain/model/Country.java
@@ -2,27 +2,25 @@ package com.challenge.cms.country.domain.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@SQLDelete(sql = "UPDATE countries SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted = false")
 @Table(name = "countries")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Country {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     private String capital;
-
     private String region;
-
     private String subRegion;
-
     private double area;
-
+    private boolean isDeleted;
 }
