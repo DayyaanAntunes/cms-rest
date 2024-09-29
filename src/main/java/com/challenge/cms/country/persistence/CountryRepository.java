5e1873debe781270c93e7068d0b945f9e93fff45
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-29 22:44:17 +0200

    feat(country): add queries to find all deleted countries and find deleted country by id

--- src/main/java/com/challenge/cms/country/persistence/CountryRepository.java
+++ src/main/java/com/challenge/cms/country/persistence/CountryRepository.java
@@ -3,6 +3,8 @@ package com.challenge.cms.country.persistence;
 import com.challenge.cms.country.domain.model.Country;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,9 @@ public interface CountryRepository extends JpaRepository<Country, Long> {
 
     List<Country> findAll(Sort sort);
     boolean existsByCapital(String capital);
+    @Query(value = "SELECT * FROM countries WHERE is_deleted = true",
+            nativeQuery = true)
+    List<Country> findByIsDeleted();
+    @Query(value = "SELECT * FROM countries WHERE is_deleted = true AND id = :deleted_id", nativeQuery = true)
+    Country findDeletedById(@Param("deleted_id") Long id);
 }
