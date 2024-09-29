a4be375e6aa94967c6dcce825bcc93b259a06e2e
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2024-09-29 22:52:59 +0200

    feat(user): add queries to find all deleted users and find deleted user by id

--- src/main/java/com/challenge/cms/user/presistence/UserRepository.java
+++ src/main/java/com/challenge/cms/user/presistence/UserRepository.java
@@ -2,9 +2,19 @@ package com.challenge.cms.user.presistence;
 
 import com.challenge.cms.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     boolean existsByEmail(String email);
-    User findUserById(Long id);
+    @Query(value = "SELECT * FROM users WHERE is_deleted = true",
+            nativeQuery = true)
+    List<User> findByIsDeleted();
+
+    @Query(value = "SELECT * FROM users WHERE is_deleted = true AND id = :deleted_id",
+            nativeQuery = true)
+    User findDeletedById(@Param("deleted_id") Long id);
 }
